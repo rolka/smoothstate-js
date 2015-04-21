191d46dc68a8575ae098339d2cfb45fe02f7ced3
Rolandas <rzabulis@yahoo.co.uk>
2015-04-21 14:31:18 +0300

    crap added, shit doesn't work, fuck..

--- functions.js
+++ functions.js
@@ -1,5 +1,16 @@
 
+$(document).ready(function(){
 
+	$('.animated').find('p').addClass('paras');
+
+	$('body').on('click', '.about', function(){
+		alert('.about clicked! From ready ' + event.target.nodeName);
+	});
+
+	$('body').find('.home p').addClass('home-p');
+	$('body').find('.about p').addClass('about-p');
+
+});
 
 
 
@@ -10,6 +21,8 @@
       content = $('#main').smoothState({
       	prefetch: true,
       	pageCacheSize: 4,
+      	development: true,
+      	
         // Runs when a link has been activated
         onStart: {
           duration: 500, // Duration of our animation
@@ -17,30 +30,55 @@
             // toggleAnimationClass() is a public method
             // for restarting css animations with a class
             content.toggleAnimationClass('is-exiting');
-            console.log('hello from render: ' + event.target.nodeName );
-            $('.about').on('click', function(){
-				alert('.about clicked!' + event.target.nodeName);
-			});
+            // console.log('hello from render: ' + event.target.nodeName );
+            // $('.about').on('click', function(){
+				// alert('.about clicked!' + event.target.nodeName);
+			// });
 
             // Scroll user to the top
             $body.animate({
               scrollTop: 0
             });
           }
-        }
+        },
+
+
+        onEnd : {
+                duration: 400, // Duration of the animations, if any.
+                render: function (url, $container, $content) {
+                    $body.css('cursor', 'auto');
+                    $body.find('a').css('cursor', 'auto');
+                    $container.html($content);
+                    // $(document).ready();
+                    // $(window).trigger('load');
+                    console.log('onEnd!');
+                    $.readyFn.execute();
+                    // $body.find('.home p').addClass('home-p');
+                    // $body.find('.about p').addClass('about-p');
+
+     //                $body.on('click', '.about', function(){
+					// 	alert('.about render clicked! ' + event.target.nodeName);
+					// });
+                }
+            },
+
+        callback : function(url, $container, $content) {
+
+        	// this the thingy
+        	$(function(){
+        		$.readyFn.execute();
+        	});
+
+        	// $.readyFn.execute();
+
+
+				// $(function() {
+				// 	$('body').on('click', '.about', function(){
+				// 		alert('.about clicked!' + event.target.nodeName);
+				// 	});
+				// });
+			}
+
       }).data('smoothState');
       //.data('smoothState') makes public methods available
 })(jQuery);
-
-
-$(function () {
-
-	$('.about').on('click', function(){
-		alert('.about clicked!' + event.target.nodeName);
-	});
-
-	
-	// alert ('ola');
-
-
-});
